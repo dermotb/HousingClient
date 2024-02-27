a2775ea7e0b7954b8a4b2dbd2836bb5e673445ef
Dermot Boyle <dermotb@hotmail.com>
2024-02-27 22:29:42 +0000

    Add project files.

---
+++ HousingClient/Models/Property.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Housing.Models
+{
+    public class Property
+    {
+        [Key]
+        public int ID {  get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int BedroomCount { get; set; }
+
+        [Required]
+        public bool FullyDelegated { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string ClientEmail { get; set; }
+
+        [Required]
+        public DateTime LeaseExpiry { get; set; }
+    }
+}
---
+++ HousingClient/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Housing
+{
+    using Housing.Models;
+    using System.Net.Http.Headers;
+    using System.Net.Http.Json;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAllHouses().Wait();
+        }
+
+        private static async Task GetAllHouses()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://housingead.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/House");
+
+            if (response.IsSuccessStatusCode) 
+            {
+                IEnumerable<Property> houses = await response.Content.ReadFromJsonAsync<IEnumerable<Property>>();
+
+                foreach(Property p in houses)
+                {
+                    Console.WriteLine(p.Address.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine(response.StatusCode+" "+response.ReasonPhrase);
+            }
+        }
+    }
+}
